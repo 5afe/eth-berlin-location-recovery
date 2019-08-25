4432a48778f4590b70b251522d24af738dc53464
Germán Martínez <germaga92@gmail.com>
2019-08-25 09:47:52 +0200

    Add form logic

---
+++ react/src/components/forms/TextField/index.js
@@ -0,0 +1,13 @@
+/*
+import React from 'react'
+import { Field } from 'react-final-form'
+
+const TextField = (props) => (
+  <Field
+    type='text'
+    { ...props }
+  />
+)
+
+export default TextField
+*/
